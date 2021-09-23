dbcf17496d92f8b9c02480bb3c8c121c0da08355
Carlos Gomez <caragomezher@unal.edu.co>
2021-09-23 16:35:19 -0500

    Initial commit

---
+++ app/src/main/java/com/diplomado/miapp/view/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.diplomado.miapp.view.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.diplomado.miapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
